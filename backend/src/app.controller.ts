b68f40a49405efd5ca20bbf2b7d3f91d3f68bf0a
Alina Fleiscmann <alina.fleischmannn@gmail.com>
2024-08-07 15:20:51 +0200

    createTrip method added (for TravelPlan app)

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('trips')
+  getTrips() {
+    return this.appService.getTrips();
+  }
+
+  @Post('trips')
+  createTrip(@Body() payload: any) {
+    return this.appService.createTrip(payload);
   }
 }
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,26 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private dummyTrips = [
+    { id: 1, name: 'Berlin Marathon', location: 'Berlin', date: '2024-08-20' },
+    { id: 2, name: 'SOAD concert', location: 'London', date: '2024-10-15' },
+    { id: 3, name: 'Berlin Marathon', location: 'Berlin', date: '2024-08-20' },
+    { id: 4, name: 'SOAD concert', location: 'London', date: '2024-10-15' },
+    { id: 5, name: 'Berlin Marathon', location: 'Berlin', date: '2024-08-20' },
+    { id: 6, name: 'SOAD concert', location: 'London', date: '2024-10-15' }
+  ]
+
   getHello(): string {
     return 'Hello World!';
+  }
+  getTrips() {
+    return this.dummyTrips;
+  }
+
+  createTrip(trip: any) {
+    const newId = this.dummyTrips.length > 0 ? Math.max(...this.dummyTrips.map(t => t.id)) + 1 : 1;
+    const newTrip = { id: newId, ...trip };
+    this.dummyTrips.push(newTrip);
+    return newTrip;
   }
 }
